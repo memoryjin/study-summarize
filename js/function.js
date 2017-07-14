32fbf51f4fd41095457693b9c5e383f4f50a954a
memoryjin <352165019@qq.com>
2017-07-14 11:51:45 +0800

    js format

--- js/function.js
+++ js/function.js
@@ -4,64 +4,64 @@
  * @param {string} [containerSelector='body'] - 容器css选择器，默认为‘body’
  * @param {string} moveSelector - 可拖动的元素选择符
  */
-const makeEleDragble = function(containerSelector='body', moveSelector) {
-	let mousedown = false;
-	let diffX, diffY;
-	const moveEle = document.querySelector(moveSelector);
-	const containerEle = document.querySelector(containerSelector);
-	moveEle.style.position = 'absolute';
-	if(getComputedStyle(containerEle).position === 'static') {
-		containerEle.style.position = 'relative';
-	}
+const makeEleDragble = function(containerSelector = 'body', moveSelector) {
+  let mousedown = false;
+  let diffX, diffY;
+  const moveEle = document.querySelector(moveSelector);
+  const containerEle = document.querySelector(containerSelector);
+  moveEle.style.position = 'absolute';
+  if (getComputedStyle(containerEle).position === 'static') {
+    containerEle.style.position = 'relative';
+  }
 
-	const getCanMoveArea = function() {
-		const maxLeft = containerEle.offsetWidth - moveEle.offsetWidth - parseInt(getComputedStyle(containerEle)['border-width']) * 2;
-		const maxTop = containerEle.offsetHeight - moveEle.offsetHeight - parseInt(getComputedStyle(containerEle)['border-width']) * 2;
-		return [maxLeft, maxTop];
-	}
-	let [maxLeft, maxTop] = getCanMoveArea();				
+  const getCanMoveArea = function() {
+    const maxLeft = containerEle.offsetWidth - moveEle.offsetWidth - parseInt(getComputedStyle(containerEle)['border-width']) * 2;
+    const maxTop = containerEle.offsetHeight - moveEle.offsetHeight - parseInt(getComputedStyle(containerEle)['border-width']) * 2;
+    return [maxLeft, maxTop];
+  }
+  let [maxLeft, maxTop] = getCanMoveArea();
 
-	const activateMove = function(e) {
-		mousedown = true;
-		diffX = e.clientX - moveEle.getBoundingClientRect().left;
-		diffY = e.clientY - moveEle.getBoundingClientRect().top;
-	}
-	const cancelMove = function() {
-		mousedown = false;
-	}
+  const activateMove = function(e) {
+    mousedown = true;
+    diffX = e.clientX - moveEle.getBoundingClientRect().left;
+    diffY = e.clientY - moveEle.getBoundingClientRect().top;
+  }
+  const cancelMove = function() {
+    mousedown = false;
+  }
 
-	const move = function(e) {
-		if(mousedown) {
-			let left = e.clientX - containerEle.getBoundingClientRect().left - diffX - parseInt(getComputedStyle(containerEle)['border-width']);
-			let top = e.clientY - containerEle.getBoundingClientRect().top - diffY - parseInt(getComputedStyle(containerEle)['border-width']);
-			if(left >= 0 && left <= maxLeft) {
-				moveEle.style.left = left + 'px';
-			} else {
-				moveEle.style.left = left < 0 ? 0 + 'px' : maxLeft + 'px';
-			}
-			if(top >= 0 && top <= maxTop) {
-				moveEle.style.top = top + 'px';
-			} else {
-				moveEle.style.top = top < 0 ? 0 + 'px' : maxTop + 'px';
-			}					
-		}
-	}
-	moveEle.addEventListener('mousedown', activateMove, false);
-	document.addEventListener('mouseup', cancelMove, false);
-	document.addEventListener('mousemove', move, false);			
+  const move = function(e) {
+    if (mousedown) {
+      let left = e.clientX - containerEle.getBoundingClientRect().left - diffX - parseInt(getComputedStyle(containerEle)['border-width']);
+      let top = e.clientY - containerEle.getBoundingClientRect().top - diffY - parseInt(getComputedStyle(containerEle)['border-width']);
+      if (left >= 0 && left <= maxLeft) {
+        moveEle.style.left = left + 'px';
+      } else {
+        moveEle.style.left = left < 0 ? 0 + 'px' : maxLeft + 'px';
+      }
+      if (top >= 0 && top <= maxTop) {
+        moveEle.style.top = top + 'px';
+      } else {
+        moveEle.style.top = top < 0 ? 0 + 'px' : maxTop + 'px';
+      }
+    }
+  }
+  moveEle.addEventListener('mousedown', activateMove, false);
+  document.addEventListener('mouseup', cancelMove, false);
+  document.addEventListener('mousemove', move, false);
 
-	return {
-		enable() {
-			moveEle.addEventListener('mousedown', activateMove, false);
-			document.addEventListener('mouseup', cancelMove, false);
-			document.addEventListener('mousemove', move, false);
-		},
-		disable() {
-			moveEle.removeEventListener('mousedown', activateMove, false);
-			document.removeEventListener('mouseup', cancelMove, false);
-			document.removeEventListener('mousemove', move, false);			
-		}
-	}	
+  return {
+    enable() {
+      moveEle.addEventListener('mousedown', activateMove, false);
+      document.addEventListener('mouseup', cancelMove, false);
+      document.addEventListener('mousemove', move, false);
+    },
+    disable() {
+      moveEle.removeEventListener('mousedown', activateMove, false);
+      document.removeEventListener('mouseup', cancelMove, false);
+      document.removeEventListener('mousemove', move, false);
+    }
+  }
 }
 
 /**
@@ -75,13 +75,13 @@ const makeEleDragble = function(containerSelector='body', moveSelector) {
  * getMaxValue([1,2,2,3,5,5,8], 3)
  */
 const getMaxValue = function(arr, index) {
-	if(!Array.isArray(arr)) {
-		return;
-	} else {
-		const newArr = [...(new Set(arr))];
-		newArr.sort((value1, value2) => value2 - value1);
-		return newArr[index - 1];
-	}
+  if (!Array.isArray(arr)) {
+    return;
+  } else {
+    const newArr = [...(new Set(arr))];
+    newArr.sort((value1, value2) => value2 - value1);
+    return newArr[index - 1];
+  }
 }
 
 /**
@@ -93,23 +93,23 @@ const getMaxValue = function(arr, index) {
  * @example
  * sortArrByRule([{person: {age: 25}}, {person: {age: 55}}, {person: {age: 5}}, {person: {age: 78}}], 'person.age', 'des')
  */
-const sortArrByRule = function(arr, desc, direction='asc') {
-	if(!Array.isArray(arr) || typeof desc !== 'string') {
-		return;
-	} else {
-		const paramsArr = desc.split('.');
-		arr.sort((value1, value2) => {
-			try {
-				paramsArr.forEach(key => {
-					value1 = value1[key];
-					value2 = value2[key];
-				});
-			} catch(e) {
-				throw new Error('the desc is incorrect!');
-			}
-			return direction === 'asc' ? value1 - value2 : value2 - value1;
-		});
-	}
+const sortArrByRule = function(arr, desc, direction = 'asc') {
+  if (!Array.isArray(arr) || typeof desc !== 'string') {
+    return;
+  } else {
+    const paramsArr = desc.split('.');
+    arr.sort((value1, value2) => {
+      try {
+        paramsArr.forEach(key => {
+          value1 = value1[key];
+          value2 = value2[key];
+        });
+      } catch (e) {
+        throw new Error('the desc is incorrect!');
+      }
+      return direction === 'asc' ? value1 - value2 : value2 - value1;
+    });
+  }
 }
 
 /**
@@ -123,11 +123,11 @@ const sortArrByRule = function(arr, desc, direction='asc') {
  * removeDuplicates([1, 2, 4], [1, 3, 5])
  */
 const removeDuplicates = function(arr1, arr2) {
-	if(!Array.isArray(arr1) || !Array.isArray(arr2)) {
-		return;
-	}
-	const initArr1 = arr1;
-	arr1 = arr1.filter(value => !arr2.includes(value));
-	arr2 = arr2.filter(value => !initArr1.includes(value));
-	return [arr1, arr2];
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return;
+  }
+  const initArr1 = arr1;
+  arr1 = arr1.filter(value => !arr2.includes(value));
+  arr2 = arr2.filter(value => !initArr1.includes(value));
+  return [arr1, arr2];
 }
